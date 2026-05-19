cef2ca62ccc4b4f1f801b5ce41d7828ad55d5592
aldrin12345 <aldrinjohnencina7@gmail.com>
2026-05-19 10:36:44 +0800

    update managepayroll

--- src/Views/Pages/Attendance/Manage/Actions/ManagePayroll.vue
+++ src/Views/Pages/Attendance/Manage/Actions/ManagePayroll.vue
@@ -50,6 +50,9 @@
                   @change="fetchPayrollData"
                   class="px-3 py-2 border border-gray-300 rounded-lg text-sm focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                 >
+                <option value="0">
+                    -- ALL --
+                  </option>
                   <option v-for="period in payrollPeriods" :key="period.id" :value="period.id">
                     {{ period.datePeriod }}
                   </option>
