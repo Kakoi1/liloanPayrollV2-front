f2a94ff2ca4bf997f941d4b5c068b141cb49b98c
Kakoi1 <lopezrolandshane@gmail.com>
2026-08-05 10:06:22 +0800

    user

--- src/Views/Pages/Users/index.vue
+++ src/Views/Pages/Users/index.vue
@@ -41,8 +41,8 @@
             <div v-show="cardExpanded" class="p-4">
               <div class="space-y-4">
                 <div>
-                  <label class="block text-sm font-medium text-gray-700 mb-1">Active Employee</label>
-                  <SearchDropdown
+                  <label class="block text-sm font-medium text-gray-700 mb-1">Full Name</label>
+                  <!-- <SearchDropdown
                     :apiEndpoint="'employee/active-list'"
                     :searchModel="searchEmp"
                     placeholder="Search for Employees..."
@@ -54,7 +54,13 @@
                     :defaultValue="selectedEmployee?.fullName"
                     formatLabel="none"
                     className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                  />
+                  /> -->
+                   <input 
+                    type="text" 
+                    placeholder="Input Full name" 
+                    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                    v-model="Users.fullname"
+                  >
                 </div>
                 
                 <div>
